9e8a0bd0747255f3c02ae89e454f8d010589b97c
HansonYu23 <hansonyu@ucsb.edu>
2021-02-12 21:35:18 -0800

    puzzle class enabled

--- PuzzleParanoia.py
+++ PuzzleParanoia.py
@@ -10,35 +10,13 @@ rows, cols = 3, 3
 length = rows*cols
 
 
-COMPLETION_REWARD = 100
-MOVE_PENALTY = 1
-NUMBER_EPISODES = 50000
-SHOW_EVERY = 5000
-MOVES_PER_EPISODE = 750
-
-epsilon = 0.75
-EPS_DECAY = 0.9999
-
-LEARNING_RATE = 0.15
-DISCOUNT = 0.95
-
-startQTable = "qTable-1613864179.pickle"
+NUMBER_EPISODES = 1
+SHOW_EVERY = 3000
+MOVES_PER_EPISODE = 25
 
 # Tile is a element in list puzzle,
 # direction is one of the following: "down", "up", "left", "right"
 # puzzle is a list of int with cols*rows elements
-
-
-def checkSolvable(array):
-    inversions = 0
-    for i in range(length):
-        for j in range(length):
-            if array[j] > array[i]:
-                inversions += 1
-    if inversions % 2 == 1:
-        return False
-    else:
-        return True
 
 
 class Puzzle:
@@ -47,12 +25,6 @@ class Puzzle:
         for i in range(rows*cols):
             self.tiles[i] = i
         random.shuffle(self.tiles)
-
-        while not checkSolvable(self.tiles):
-            self.tiles = [0 for x in range(rows * cols)]
-            for i in range(rows*cols):
-                self.tiles[i] = i
-            random.shuffle(self.tiles)
 
     def moveTile(self, direction):
         # find index
@@ -110,114 +82,42 @@ class Puzzle:
                 print(self.tiles[i*cols+j], end=" ")
             print()
 
-    def state(self):
-        return tuple(self.tiles)
-
 
 # Real code
 # initialize qTable
-qTable = {}
-state = [0 for i in range(length)]
-
-
-def initQTable(r):
-    if len(r) == 1:
-        state[-1] = r[0]
-        qTable[tuple(state)] = [np.random.uniform(-5, 0) for i in range(4)]
-        return
-
-    for i in r:
-        state[length - len(r)] = i
-        newRange = list(r)
-        newRange.remove(i)
-        initQTable(newRange)
-
-
-if startQTable is None:
-    ran = [i for i in range(length)]
-    initQTable(ran)
-else:
-    with open(startQTable, "rb") as f:
-        qTable = pickle.load(f)
-
-episodeRewards = []
 for i in range(NUMBER_EPISODES):
 
     # make puzzle
     puzzle = Puzzle()
 
-    # render or not
-    if i % SHOW_EVERY == 0:
-        show = True
-    else:
-        show = False
-
-    episodeReward = 0
-
     for j in range(MOVES_PER_EPISODE):
-        # get state
-        obs = puzzle.state()
+        # decide to render or not
 
         # choose an action, based on epsilon
-        if np.random.random() > epsilon:
-            action = np.argmax(qTable[obs])
-        else:
-            action = np.random.randint(0, 4)
+        action = np.random.randint(0, 4)
 
         # take the action
         puzzle.moveTile(action)
 
         # calculate reward
-        if puzzle.tiles == [1, 2, 3, 4, 5, 6, 7, 8, 0]:
-            reward = COMPLETION_REWARD
-        else:
-            reward = -MOVE_PENALTY
-
-        # get state after taking action
-        newObs = puzzle.state()
-        maxFutureQ = np.max(qTable[newObs])
-        currentQ = qTable[obs][action]
 
         # update q
-        if reward == COMPLETION_REWARD:
-            newQ = COMPLETION_REWARD
-        else:
-            newQ = (1 - LEARNING_RATE) * currentQ + \
-                LEARNING_RATE * (reward + DISCOUNT * maxFutureQ)
-        qTable[obs][action] = newQ
 
-        episodeReward += reward
+        # render
 
         # check if done
         # if done, break
-        if reward == COMPLETION_REWARD:
-            print(f"We got a success on episode {i}")
-            break
 
-    if show:
-        print('--------------')
         puzzle.printPuzzle()
+        print(f'We are taking this: {action}')
+        print("-------------------")
 
     # add the reward to the list of reward
-    episodeRewards.append(episodeReward)
 
     # epsilon decay
-    epsilon *= EPS_DECAY
 
-# average rewards
-movingAvg = np.convolve(episodeRewards, np.ones(
-    (SHOW_EVERY,))/SHOW_EVERY, mode='valid')
+# averge rewards
 
 # plot rewards
-plt.plot([i for i in range(len(movingAvg))], movingAvg)
-plt.ylabel(f"Reward {SHOW_EVERY}ma")
-plt.xlabel("episode #")
-plt.show()
 
 # save qTable
-if startQTable == None:
-    with open(f"qTable-{int(time.time())}.pickle", "wb") as f:
-        pickle.dump(qTable, f)
-else:
-    with open(startQTable, "wb") as f:
-        pickle.dump(qTable, f)
